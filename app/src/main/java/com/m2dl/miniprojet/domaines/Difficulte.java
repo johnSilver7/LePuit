527c5c638fed60e7d536946c99428eef50b4af9d
yanxkz <mayanxkz@hotmail.com>
2016-01-28 09:38:58 +0100

    Merge branch 'master' of /home/yan/AndroidStudioProjects/LePuit with conflicts.

--- app/src/main/java/com/m2dl/miniprojet/domaines/Difficulte.java
+++ app/src/main/java/com/m2dl/miniprojet/domaines/Difficulte.java
@@ -7,4 +7,13 @@ public enum Difficulte {
 
     FACILE, MOYEN, DIFFICILE;
 
+    public static int getValeur(Difficulte difficulte) {
+        switch (difficulte) {
+            case FACILE: return 1;
+            case MOYEN: return 2;
+            case DIFFICILE: return 3;
+        }
+        return 0;
+    }
+
 }
