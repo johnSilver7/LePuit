ae40074e26c84f73610e3e1f8b11c9aaa81aec86
jak09 <marquette.quentin@orange.fr>
2016-01-28 10:19:36 +0100

    update

--- app/src/main/java/com/m2dl/miniprojet/activites/JeuActivity.java
+++ app/src/main/java/com/m2dl/miniprojet/activites/JeuActivity.java
@@ -2,11 +2,19 @@ package com.m2dl.miniprojet.activites;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by yan on 28/01/16.
  */
 public class JeuActivity extends Activity {
+
+    private ImageView iPhoto;
+    private Button bPause;
+    private TextView tScore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/m2dl/miniprojet/activites/MainActivity.java
+++ app/src/main/java/com/m2dl/miniprojet/activites/MainActivity.java
@@ -1,7 +1,10 @@
 package com.m2dl.miniprojet.activites;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickJouer(View v) {
+        //startActivity(new Intent(this, SalonActivity.class));
+        startActivity(new Intent(this, JeuActivity.class));
+    }
+
+    @Override
+    public void onBackPressed() {
+        // rien
     }
 }
